66f9971deaf0b5ff6f7d39059bc038b4d2729130
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-08-17 06:40:57 -0400

    adds auth page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,3 +1,123 @@
 import React from "react";
 
 import useChangeInputConfig from "../hooks/useInput";
+import useFetchAPI from "../hooks/useFetchApi";
+import checkAuthCookie from "../hooks/checkAuthCookie";
+
+function Auth(props) {
+   let isLoginRoute = props.match.path === "/login";
+   let buttonTitle = isLoginRoute ? "Login" : "Sign up";
+   let apiURL = isLoginRoute ? "/users/login" : "/users/create-user";
+
+   const { checkIfCookieExists } = checkAuthCookie();
+
+
+   const [
+      { isLoading, response, error, setResponse },
+      handleAPICallButtonSubmit,
+      isMessageOpen,
+      handleMessageClose,
+      successMessage
+   ] = useFetchAPI(apiURL)
+   
+   const [
+      email,
+      handleEmailChange,
+      isEmailError,
+      emailErrorMessage,
+      isEmailDisabled,
+      clearEmailInput,
+   ] = useChangeInputConfig("email");
+
+   const [
+      username,
+      handleUsernameChange,
+      isUsernameError,
+      usernameErrorMessage,
+      isUsernameDisabled,
+      clearUsernameInput,
+   ] = useChangeInputConfig("username");
+
+   const [
+      password,
+      handlePasswordChange,
+      isPasswordError,
+      passwordErrorMessage,
+      isPasswordDisabled,
+      clearPasswordInput,
+   ] = useChangeInputConfig("password");
+   
+   function handleOnSubmit(e) {
+      e.preventDefault();
+
+      const user = isLoginRoute
+         ? { email, password }
+         : { email, username, password };
+
+      handleAPICallButtonSubmit({
+         method: "post",
+         data: {
+            ...user,
+         },
+      });
+   }
+
+   if (response === "user created") {
+      clearEmailInput();
+      clearUsernameInput();
+      clearPasswordInput();
+      setResponse(null);
+   }
+
+   if (checkIfCookieExists()) {
+      props.history.push("/protected");
+   }
+
+   return (
+      <div>
+         {successMessage && successMessage()}
+         {error && emailErrorMessage()}
+         <form onSubmit={handleOnSubmit}>
+            
+            <input 
+               label="Email"
+               name="email"
+               value={email}
+               onChange={handleEmailChange}
+            />
+            {isEmailError && emailErrorMessage}
+
+            <input 
+               label="Username"
+               name="username"
+               value={username}
+               onChange={handleUsernameChange}
+            />
+            {isUsernameError && usernameErrorMessage}
+
+            <input 
+               label="Password"
+               name="password"
+               value={password}
+               onChange={handlePasswordChange}
+            />
+            {isPasswordError && passwordErrorMessage}
+
+            <button
+               type="submit"
+               disabled={
+                  isLoginRoute
+                     ? isEmailDisabled || isPasswordDisabled
+                     :isEmailDisabled || isPasswordDisabled ||
+                     isUsernameDisabled
+               }
+            >
+               {buttonTitle}
+            </button>
+
+         </form>
+      </div>
+   )
+}
+
+export default Auth;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+   return (
+      <div className="home">
+         < img src = "https://pics.clipartpng.com/midle/House_PNG_Clip_Art-2146.png" alt ="house" />
+      </div>
+   )
+}
+
+export default Home;
