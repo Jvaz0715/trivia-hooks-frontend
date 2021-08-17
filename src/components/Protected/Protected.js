5c441a3f58ee9ed13e808c974a9a173d44d3facf
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-08-17 18:54:31 -0400

    Updates protected.js

--- src/components/Protected/Protected.js
+++ src/components/Protected/Protected.js
@@ -13,6 +13,11 @@ function Protected() {
    const [wins, setWins] = useState("");
    const [losses, setLosses] = useState("");
 
+   // using this to test if update on front end works well
+   const [pointsToAdd, setPointsToAdd] = useState(0);
+   const [winsToAdd, setWinsToAdd] = useState(0);
+   const [lossesToAdd, setLossesToAdd] = useState(0);
+
    let jwtCookie = Cookies.get("jwt-cookie")
    let decodedJwtCookie = jwtDecode(jwtCookie)
 
@@ -30,6 +35,20 @@ function Protected() {
       setLosses(foundUser.data.foundUser.losses)
    };
 
+   async function updatePlayerStats() {
+      let updatedUser = await axios.put(
+         `http://localhost:3001/api/users/update-player-stats/${decodedJwtCookie.id}`,
+         {
+            totalPoints: Number(totalPoints) + 8,
+            wins: Number(wins) + 21,
+            losses: Number(losses) + 1990,
+         }
+      )
+      
+      console.log(updatedUser);
+      getUser()
+   }
+
    return (
       <>
       <div className="player-stats-container">
@@ -39,6 +58,9 @@ function Protected() {
          <br/>
          Losses: {losses}
       </div>
+      {/* testing to make sure frontend update works */}
+      <button onClick={()=> updatePlayerStats()}>Update stats!</button>
+
       </>
    )
 }
