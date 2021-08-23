c461d22f5c5863915d058215e545b658b42221b7
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-08-23 15:54:20 -0400

    Fixes home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import checkAuthCookie from "../hooks/checkAuthCookie";
 import "./Home.css";
 
 function Home() {
+   const { checkIfCookieExists } = checkAuthCookie();
+   let isLoggedIn = checkIfCookieExists();
    return (
       <div className="home">
-         HOME
+         {!isLoggedIn && (
+            <>
+               <Link to="/sign-up" >Sign up</Link> or <Link to="/login">Login</Link>
+               
+            </>
+         )}
+         {isLoggedIn && (
+            <>
+               <Link to="/protected">Play Now</Link>
+            </>
+         )}
       </div>
    )
 }
